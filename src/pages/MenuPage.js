737a745816eb05c3a82710e6c0d2c4d0a5b94012
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-11-28 15:23:25 +0400

    change

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,20 +1,15 @@
 import {MenuTop} from "../components/MenuTop/MenuTop";
 
 import {AppFooter} from "../components/Footer/Footer";
-import AppTable from "../components/PNL/DataTable";
+
 
 export const MenuPage = () => {
     return (
 
             <Screen>
                 <MenuTop/>
-                <h1>Таблица сделок</h1>
-                <AppTable/>
-
-
+                <h1>какой-то компонент</h1>
                 <AppFooter/>
             </Screen>
-
-
     )
 }
--- src/pages/TradePage.js
+++ src/pages/TradePage.js
@@ -1,12 +1,13 @@
 import {MenuTop} from "../components/MenuTop/MenuTop";
 import {AppFooter} from "../components/Footer/Footer";
-import MyDataGrid from "../components/Trade/DataGrid";
+
+import MyGrid from "../components/Trade/DataGrid";
 
 export const TradePage = () => {
     return (
         <>
             <MenuTop/>
-            <MyDataGrid/>
+            <MyGrid/>
             <AppFooter/>
         </>
 

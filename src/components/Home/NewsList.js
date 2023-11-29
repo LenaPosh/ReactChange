c26c25a52d7d74834b60de5318cdbe0ac95ba07e
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-11-29 21:13:20 +0400

    check

--- src/components/Home/NewsList.js
+++ src/components/Home/NewsList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoIdCardOutline } from "react-icons/io5";
 import './style.css'
 
 const NewsList = ({ news }) => {
@@ -6,11 +7,12 @@ const NewsList = ({ news }) => {
         <div className="news-list">
             {news && news.map((item) => (
                 <div key={item.id} className="news-card">
-                    <img src={item.image} alt={item.title} />
-                    <h3>{item.title}</h3>
+
+                    <IoIdCardOutline size={50} color="black" />
+                    <h3 style={{ color: 'rgba(0,0,0,0.82)' }}>{item.title}</h3>
                     <p>{item.description}</p>
                     <span>{item.date}</span>
-                    <a href={item.link}>Читать далее</a>
+                    <a href={item.link} style={{ color: 'rgba(0, 0, 0, 0.51)' }}>Читать далее</a>
                 </div>
             ))}
         </div>

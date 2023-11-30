e72fa6a2360aa885add792c2bcb3ab0ce3048346
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-11-30 13:14:25 +0400

    active home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import { useTranslation } from 'react-i18next'; // Импортируем useTranslation
+import { useTranslation } from 'react-i18next';
 import './style.css';
 import { FaWallet } from 'react-icons/fa';
 import { TiChartBarOutline } from 'react-icons/ti';
 import { ImHome } from 'react-icons/im';
-// import { AiOutlineLineChart } from "react-icons/ai";
 import { IoBarChartSharp } from "react-icons/io5";
-// import { FcComboChart } from "react-icons/fc";
-// import { FaChartBar } from "react-icons/fa";
 
 export const Screen = styled.div`
   min-height: 100vh;
@@ -27,6 +24,7 @@ const FooterStyle = styled.footer`
   width: 100vw;
   height: 60px;
 `;
+
 const MenuLink = styled(NavLink)`
   color: ${props => props.isHomePage ? '#08a652' : '#495057'};
   text-decoration: none;
@@ -79,29 +77,27 @@ const MenuLink = styled(NavLink)`
     transition: height 0.3s, background 0.3s;
   }
 `;
+
 export const AppFooter = () => {
     const { t } = useTranslation();
     const [isHomePage, setIsHomePage] = useState(false);
-    useEffect(() => {
+
+    useLayoutEffect(() => {
         const currentPath = window.location.pathname.startsWith('/home');
         setIsHomePage(currentPath);
     }, []);
-
-
 
     return (
         <>
             <FooterStyle className="footer">
                 <div className="menu-navigation">
                     <div className="menu-items">
-
                         <MenuLink to="/home" className={`menu-item ${isHomePage ? 'active' : ''}`} activeClassName="active">
                             <div className="menu-icon">
                                 <ImHome />
                             </div>
                             <div className="menu-text" style={{ whiteSpace: 'nowrap' }}>{t('home')}</div>
                         </MenuLink>
-
 
                         <NavLink to="/menu" className="menu-item">
                             <div className="menu-icon">
@@ -116,12 +112,6 @@ export const AppFooter = () => {
                             </div>
                             <div className="menu-text">{t('trade')}</div>
                         </NavLink>
-                        {/*<NavLink to="/trade" className="menu-item">*/}
-                        {/*    <div className="menu-icon">*/}
-                        {/*        <TiChartBarOutline />*/}
-                        {/*    </div>*/}
-                        {/*    <div className="menu-text">{t('qr_code_title.part_1')}</div>*/}
-                        {/*</NavLink>*/}
 
                         <NavLink to="/wallet" className="menu-item">
                             <div className="menu-icon">

02657a4daf983612d56660e26a926a00f6be3c13
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-11-30 14:08:04 +0400

    active home

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,34 +1,38 @@
-import { createBrowserRouter } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { MenuPage } from "./pages/MenuPage";
-import { TradePage } from "./pages/TradePage";
-import { WalletPage } from "./pages/WalletPage";
-import { BuyCryptoPage } from "./pages/BuyCryptoPage";
-import { Navigate } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {MenuPage} from "./pages/MenuPage";
+import {TradePage} from "./pages/TradePage";
+import {WalletPage} from "./pages/WalletPage";
+import {BuyCryptoPage} from "./pages/BuyCryptoPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Navigate to="/home" />,
-    },
-    {
-        path: "/menu",
-        element: <MenuPage />,
-    },
-    {
-        path: "/home",
-        element: <HomePage />,
-    },
-    {
-        path: "/trade",
-        element: <TradePage />,
-    },
-    {
-        path: "/wallet",
-        element: <WalletPage />,
-    },
-    {
-        path: "/buyCrypto",
-        element: <BuyCryptoPage />,
-    },
-]);
+        {
+            path: "/",
+            element: <HomePage />
+        },
+
+        {
+            path: "/menu",
+            element: <MenuPage />
+        },
+        {
+            path: "/home",
+            element: <HomePage />
+        },
+        {
+            path: "/trade",
+            element: <TradePage />
+        },
+        {
+            path: "/wallet",
+            element: <WalletPage />
+        },
+        {
+            path: "/buyCrypto",
+            element: <BuyCryptoPage />
+        }
+
+
+
+    ],
+);
